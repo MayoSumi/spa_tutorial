576db6e3baaad09559b34049e0ca29a7eb2218e4
Mayo Sumi <>
2021-08-01 22:57:39 +0900

    fix 認証状態に応じてナビバーとフッターを出し分ける

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,13 +1,23 @@
 <template>
     <footer class="footer">
-        <button class="button button--link" @click="logout">Logout</button>
-        <RouterLink class="button button--link" to="/login">
+        <!-- ログイン済みの場合 -->
+        <button v-if="isLogin" class="button button--link" @click="logout">
+            Logout
+        </button>
+
+        <!-- 未ログインの場合 -->
+        <RouterLink v-else class="button button--link" to="/login">
             Login / Register
         </RouterLink>
     </footer>
 </template>
 <script>
 export default  {
+    computed: {
+        isLogin() {
+            return this.$store.getters['auth/check']
+        }
+    },
     methods: {
         async logout() {
             await this.$store.dispatch('auth/logout')
