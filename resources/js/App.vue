f640190633ef1e62f80d3804d9e4d11d910a2445
Mayo Sumi <>
2021-08-15 15:22:54 +0900

    fix パスを修正

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -15,7 +15,7 @@
 <script>
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
-import { INTERNAL_SERVER_ERROR } from '.util'
+import { INTERNAL_SERVER_ERROR } from './util'
 
 
 export default {
